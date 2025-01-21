6af5f0d3259395e4f16ad1df04a6f90a1f1c9de7
subahan ali <subahanislam523@gmail.com>
2025-01-21 13:49:39 +0600

    new typescritp

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-var userName = "Subahan Ali";
-console.log(userName);
+function addNumbers(num1, num2, num3) {
+    console.log(num1 * num2 / num3);
+}
+addNumbers(42, 34, 55);
+addNumbers(42, 34, 99);
--- index.ts
+++ index.ts
@@ -1,2 +1,6 @@
-let userName = "Subahan Ali"
-console.log(userName)
+function addNumbers(num1:number , num2:number,num3:number ){
+    console.log(num1*num2/num3)
+
+}
+addNumbers(42,34,55) ;
+addNumbers(42,34,99) ;
