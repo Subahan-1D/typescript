addeb9b66cbb7eb987e1e98efb8b70d0dea563cd
subahan ali <subahanislam523@gmail.com>
2025-01-22 20:21:24 +0600

    addded to by built in data type

---
+++ built-in-type/index.js
@@ -0,0 +1,12 @@
+// built in data type : number , string , void , boolean , void, undefined , null 
+var userId;
+var fullName;
+var firstName;
+var lastName;
+var isAdmin;
+userId = 100;
+firstName = "Subahan";
+lastName = "Ali";
+isAdmin = true;
+fullName = firstName.concat(lastName);
+console.log("Your Id :  ".concat(userId, " Your Name : ").concat(fullName, " is Admin Access : ").concat(isAdmin));
---
+++ built-in-type/index.ts
@@ -0,0 +1,15 @@
+// built in data type : number , string , void , boolean , void, undefined , null 
+
+let userId : number;
+let fullName : string ;
+let firstName: string ;
+let lastName: string ;
+let isAdmin : boolean ;
+ 
+userId= 100 ;
+firstName="Subahan" ;
+lastName ="Ali";
+isAdmin = true;
+fullName = firstName.concat( lastName)
+
+console.log(`Your Id :  ${userId} Your Name : ${fullName} is Admin Access : ${isAdmin}`)
