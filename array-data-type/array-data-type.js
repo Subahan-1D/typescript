20a74913e299168e7fc9f430987199a1b5256917
subahan ali <subahanislam523@gmail.com>
2025-01-24 11:46:55 +0600

    added to sort pop shift unshift

---
+++ array-data-type/array-data-type.js
@@ -0,0 +1,11 @@
+var userName;
+userName = ["subahan", "juwel", "imran"];
+console.log(userName[0]);
+userName.sort();
+userName.push("Sohel");
+// userName.shift()
+userName.unshift("abarul");
+console.log(userName);
+var multiplyArray;
+multiplyArray = [22, "thakurgaon"];
+console.log(multiplyArray);
---
+++ array-data-type/array-data-type.ts
@@ -0,0 +1,16 @@
+
+
+let userName: string[]  ;
+userName = ["subahan", "juwel", "imran"]
+console.log(userName[0])
+userName.sort()
+userName.push("Sohel")
+// userName.shift()
+
+userName.unshift("abarul")
+
+console.log(userName)
+
+let multiplyArray : (string|number)[];
+multiplyArray=[22,"thakurgaon"] ;
+console.log(multiplyArray)
